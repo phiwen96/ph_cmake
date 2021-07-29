d5e2332ec30c5d24a214e7a69c9ea3f649132df6
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-29 12:01:06 +0200

    a

--- cmake/fetch.cmake
+++ cmake/fetch.cmake
@@ -1,25 +1,44 @@
 macro (ph_fetch )
 	set (prefix ARG)
 	set (NoArg)
-	set (SingleArg)
-	set (MultiArg)
+	set (SingleArg USER VERSION)
+	set (MultiArg REPOS)
 
 	cmake_parse_arguments (${prefix} "${NoArg}" "${SingleArg}" "MultiArg" ${ARGN})
 
 	
-
-	if (NOT TARGET ${target})
-		include (FetchContent)
-		FetchContent_Declare(
-			${target}
-			GIT_REPOSITORY https://github.com/phiwen96/${target}.git
-			GIT_TAG        v0.0.0)
-
-		FetchContent_GetProperties (${target})
-		
-		if (NOT {target}_POPULATED)
-			FetchContent_Populate (${target})
-			add_subdirectory (${${target}_SOURCE_DIR} ${${target}_BINARY_DIR})
-		endif ()
+	if (NOT ARG_REPOS)
+		message (FATAL_ERROR "You must specify [1, -∞) project names.")
 	endif ()
+
+	if (NOT ARG_USER)
+		message (FATAL_ERROR "You must specify a user for use in the urls of repos, for example phiwen96.")
+	endif ()
+
+	if (NOT ARG_VERSION)
+		message (FATAL_ERROR "You must specify a version for .")
+	endif ()
+
+
+	foreach (git_repo IN LISTS ARG_REPOS)
+	 
+		if (NOT TARGET ${git_repo})
+
+			message ("Fetching git repo ${git_repo}")
+			include (FetchContent)
+
+			FetchContent_Declare(
+				${git_repo}
+				GIT_REPOSITORY https://github.com/${ARG_USER}/${git_repo}.git
+				GIT_TAG        ${ARG_VERSION})
+
+			FetchContent_GetProperties (${target})
+			
+			if (NOT ${git_repo}_POPULATED)
+				FetchContent_Populate (${git_repo})
+				add_subdirectory (${${git_repo}_SOURCE_DIR} ${${git_repo}_BINARY_DIR})
+			endif ()
+		endif ()
+
+	endforeach ()
 endmacro ()
