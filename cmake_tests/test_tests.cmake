b5971ee5bb23af21c215f208375747eec172ec73
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-07-29 14:00:37 +0200

    a

---
+++ cmake_tests/test_tests.cmake
@@ -0,0 +1,33 @@
+message ("hej")
+
+# ph_create_tests (
+
+# 	MODULES 
+# 		"concepts"
+# 		"common"
+# 		"Floating"
+# 		"Integer"
+# 		"Signed"
+# 		"Unsigned"
+# 		"Number"
+# 		"Char"
+# 		"Iterator"
+# 		"Range"
+# 		"String"
+# 		"Integral"
+# 		"Arithmetic"
+# 		"Fundamental"
+# 		"Void"
+# 		"Pointer"
+# 		"Size"
+# 		"Function"
+# 		"Reference"
+# 		"Array"
+# 		"uni"
+
+# 	INCLUDE
+# 		"${include_dir}"
+
+# 	SRC 
+# 		"${src_dir}"
+# )
