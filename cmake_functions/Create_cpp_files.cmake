32fef332ce9370aadc004a39fcf87987c54ea9a8
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-02 11:16:28 +0200

    a

--- cmake_functions/Create_cpp_files.cmake
+++ cmake_functions/Create_cpp_files.cmake
@@ -3,7 +3,7 @@ macro (Create_cpp_files)
 	set (prefix ARG)
 	set (NoValues)
 	set (SingleValues INCLUDE_DIR SRC_DIR TEXT)
-	set (MultiValues FILES VISITORS)
+	set (MultiValues FILES PRE_VISITORS POST_VISITORS)
 
 	cmake_parse_arguments (${prefix} "${NoValues}" "${SingleValues}" "${MultiValues}" ${ARGN})
 
@@ -30,6 +30,17 @@ macro (Create_cpp_files)
 		set (ARG_TEXT "")
 	endif ()
 
+	macro (pre_visitor file)
+		message("PRE_VISITOR ${file}")
+	endmacro ()
+
+	macro (post_visitor file)
+			message("POST_VISITOR ${file}")
+	endmacro ()
+
+	Files (FILES ${files} TEXT ${ARG_TEXT} PRE_VISITORS ${ARG_PRE_VISITORS} pre_visitor POST_VISITORS ${ARG_POST_VISITORS} post_visitor)
+
+
 
 	foreach (filename IN LISTS ARG_FILES)
 		list (APPEND files "${ARG_INCLUDE_DIR}/${filename}.hpp" "${ARG_SRC_DIR}/${filename}.cpp")
@@ -41,6 +52,5 @@ macro (Create_cpp_files)
 
 	
 
-	Files (FILES ${files} TEXT ${ARG_TEXT} VISITORS ${ARG_VISITORS})
 	
 endmacro ()
