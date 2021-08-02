b0b321a2ebda6ddd6131566e53b78970ada5c428
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-02 10:28:45 +0200

    a

--- cmake_functions/Create_cpp_executables.cmake
+++ cmake_functions/Create_cpp_executables.cmake
@@ -7,29 +7,14 @@ macro (Create_cpp_executables)
 
 	cmake_parse_arguments (${prefix} "${NoValues}" "${SingleValues}" "${MultiValues}" ${ARGN})
 
-	
-	if (NOT ARG_INCLUDE_DIR)
-		set (ARG_INCLUDE_DIR "include")
-		# message (FATAL_ERROR "Please give us a include dir.")
-	endif ()
 
-	if (NOT ARG_SRC_DIR)
-		set (ARG_SRC_DIR "src")
-	endif ()
 
-	if (NOT ARG_FILES)
-		message (FATAL_ERROR "Please give us a FILE.")
-	endif ()
-
-	if (NOT ARG_FILES)
-		message (FATAL_ERROR "Please give us a FILE.")
-	endif ()
 
 	if (NOT ARG_TEXT)
 		set (ARG_TEXT "")
 	endif ()
 
-	Files (DIR "/src" FILES ${ARG_TARGETS})
+	Files (DIR "src" TEXT ${ARG_TEXT} FILES ${ARG_TARGETS})
 
 
 	foreach (filename IN LISTS ARG_TARGETS)
