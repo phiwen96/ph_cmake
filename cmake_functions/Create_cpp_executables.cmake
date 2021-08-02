a739e8aa31f6b969981363385748604516cd02ca
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-02 11:02:59 +0200

    a¨

--- cmake_functions/Create_cpp_executables.cmake
+++ cmake_functions/Create_cpp_executables.cmake
@@ -3,7 +3,7 @@ macro (Create_cpp_executables)
 	set (prefix ARG)
 	set (NoValues)
 	set (SingleValues INCLUDE_DIR SRC_DIR)
-	set (MultiValues TARGETS)
+	set (MultiValues TARGETS PRE_VISITOR POST_VISITOR)
 
 	cmake_parse_arguments (${prefix} "${NoValues}" "${SingleValues}" "${MultiValues}" ${ARGN})
 
@@ -14,17 +14,30 @@ macro (Create_cpp_executables)
 		set (ARG_TEXT "")
 	endif ()
 
-	Files (DIR "src" TEXT ${ARG_TEXT} FILES ${ARG_TARGETS})
+	macro (pre_visitor file)
+		message("PRE_VISITOR ${file}")
+	endmacro ()
+
+	macro (post_visitor file)
+			message("POST_VISITOR ${file}")
+	endmacro ()
+	
+
+	Files (DIR "src" TEXT ${ARG_TEXT} FILES ${ARG_TARGETS} PRE_VISITOR pre_visitor POST_VISITOR post_visitor)
 
 
-	foreach (filename IN LISTS ARG_TARGETS)
+	# foreach (target IN LISTS ARG_TARGETS)
 
-		if (NOT TARGET ARG_INCLUDE_DIR)
-			add_executable (${filename} "src/${filename}.cpp")
-			# list (APPEND files "include/${filename}.cmake" "src/${filename}.cmake")
-		endif ()
 
-	endforeach ()
+
+	# 		add_executable (${target} "src/${filename}.cpp")
+
+	# 	if (NOT TARGET ARG_INCLUDE_DIR)
+	# 		add_executable (${filename} "src/${filename}.cpp")
+	# 		# list (APPEND files "include/${filename}.cmake" "src/${filename}.cmake")
+	# 	endif ()
+
+	# endforeach ()
 
 	# Create_files (FILES ${files} TEXT ${ARG_TEXT})
 
