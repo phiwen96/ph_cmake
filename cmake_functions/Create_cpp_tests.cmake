5ffb155e4e564b41ed0a20e181a56fa9b213fb82
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-08-03 14:48:40 +0200

    a

--- cmake_functions/Create_cpp_tests.cmake
+++ cmake_functions/Create_cpp_tests.cmake
@@ -1,14 +1,18 @@
-if (NOT TARGET Catch2)
-	include (FetchContent)
 
-	FetchContent_Declare(
-		Catch2
-		GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-		GIT_TAG        v2.13.1)
-
-	FetchContent_MakeAvailable (Catch2)
-endif ()
 
 macro (Create_cpp_tests)
-	ph_parse ()
+
+	if (NOT TARGET Catch2)
+		include (FetchContent)
+
+		FetchContent_Declare(
+			Catch2
+			GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+			GIT_TAG        v2.13.1)
+
+		FetchContent_MakeAvailable (Catch2)
+	endif ()
+
+
+
 endmacro ()
